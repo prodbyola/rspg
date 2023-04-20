2b8ecbd6312710b9ee30f7b2654fc590b0f10e1e
Ajayi Olamide <prodbyola@gmail.com>
2023-04-20 14:18:50 +0100

    First iteration.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 
+/// Convert a decimal unsigned integer to its base16 representation.
+/// Author: Ajayi Olamide
 pub fn dec_to_hex(input: usize) -> Result<String, Box<dyn Error>> {
     let alpha = ["a", "b", "c", "d", "e", "f"]; // base 16 alphabets
     let mut result = String::new();
@@ -10,13 +12,16 @@ pub fn dec_to_hex(input: usize) -> Result<String, Box<dyn Error>> {
         let q = i / c;
         let r = i % c; // remainder of each division
 
+      	// final char to add to result
         let mut char = r.to_string().chars().next().ok_or("Failed to convert char")?;
 
+        // if the char is beyond base10, get its base16 equivalent
         if r > 9 {
             let a = alpha[r - 10];
             char = a.chars().next().ok_or("Failed to convert char")?;
         }
 
+        // insert the char
         result.insert(0, char);
 
         i = q;
@@ -32,7 +37,6 @@ mod tests {
     #[test]
     fn test() {
         let result = dec_to_hex(62).expect("unable to convert");
-        println!("result {}", dec_to_hex(188).expect("unable to convert"));
         assert_eq!(result.as_str(), "3e");
     }
 }
